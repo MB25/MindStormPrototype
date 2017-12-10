ffd77f0d042df4257ed4dda3e985ea8d030ac359
Markus Bader <a1404709@unet.univie.ac.at>
2017-12-10 22:14:11 +0100

    finally!!

--- MainServer/src/mainServer/MainServer.java
+++ MainServer/src/mainServer/MainServer.java
@@ -2,6 +2,8 @@ package mainServer;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,48 +13,96 @@ import java.net.Socket;
 **/
 public class MainServer {
 	public static void main(String[] args) throws Exception	{
-		
-		  String clientSentence;
-		  ServerSocket welcomeSocket = new ServerSocket(6789); //Chose unused Socket
+	      System.out.print("running..");
 		  int [] values = new int[5];							//For storing the finger-values
-		  
-		  while (true) {
-		  Socket connectionSocket = welcomeSocket.accept();		//wait for connection
-		   
-		   
-		   BufferedReader inFromClient =
-		    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));   
-		   clientSentence = inFromClient.readLine().toString();
-		   
-		   System.out.println("Received: " + clientSentence);
-		   values = decodeReceivedString(clientSentence);
-		   
-		   for(int i = 0; i<values.length; i++){
-			   System.out.println(i + ":" + values[i]);
-		   }
-		   
+		  DatagramSocket serverSocket = new DatagramSocket(9876);
+		  byte[] recData = new byte[20];
+		  while(true){
+			  DatagramPacket receivePacket = new DatagramPacket(recData, recData.length);
+              serverSocket.receive(receivePacket);
+              String sentence = new String( receivePacket.getData());
+              System.out.println("RECEIVED: " + sentence);
+              
+              values = decodeReceivedString(sentence);
+              /*
+              for(int i = 0; i<5; i++){
+            	  System.out.println("testprint..");
+            	  System.out.println(i +": "+ values[i]);
+              }
+              */
+              String gesture ="";
+	   			switch(getGesture(values)){
+   		   		case 1: gesture = "Left";
+   		   		break;
+   		   		case 2: gesture = "Forward";
+   		   		break;
+   		   		case 3: gesture = "Right";
+   		   		break;
+   		   		case 4: gesture = "Stop";
+   		   		break;
+   		   		default: gesture = "no gesture detected";
+   		   }
+	   			System.out.println("gesture: " + gesture);
 		  }
-		  //source: https://systembash.com/a-simple-java-tcp-server-and-tcp-client/
 		  
-		}
+		  
+		 }
 	
 	/**
 	 * For converting received string to an array
 	 * @param recStr = The String received 
 	 * @return integer array with extracted values
 	 */
+	
+	private static int getGesture(int[] values){
+		int threshold = 100;
+		int id = -1;
+		
+		if(values[0] <= threshold && values[1] >= threshold && values[2] >= threshold 
+				&& values[3] >= threshold && values[4] >= threshold) return 1;
+		
+		if(values[0] >= threshold && values[1] <= threshold && values[2] >= threshold 
+				&& values[3] >= threshold && values[4] >= threshold) return 2;
+		
+		if(values[0] >= threshold && values[1] >= threshold && values[2] >= threshold 
+				&& values[3] >= threshold && values[4] <= threshold) return 3;
+		
+		if(values[0] >= threshold && values[1] >= threshold && values[2] >= threshold 
+				&& values[3] >= threshold && values[4] >= threshold) return 4;
+		
+				
+		/*
+		if(values[0] >= threshold){
+			if(values[1] >= threshold && values[2] >= threshold && 
+					values[3] >= threshold && values[4] >= threshold){
+				return 4;
+			}
+			return 1;
+		}
+		if(values[1] >= threshold){
+			if(values[2] >= threshold){
+				return 2;
+			}
+				
+		}
+		if(values[4] >= threshold) return 3;
+		*/
+		return id;
+	}
+	
 	private static int[] decodeReceivedString(String recStr){
 		int[] arr = new int[5];
 		int idx = 0;
 		String h = "";
+		int counted = 0;
 		//System.out.println(recStr.length());
-		for(int j = 0; j<recStr.length(); j++){	//iterates through string until it finds a ",". Then it stores all the values since the last ","
+		for(int j = 0; counted < 5; j++){	//iterates through string until it finds a ",". Then it stores all the values since the last ","
 			char cur = recStr.charAt(j);		//as one number in the values array
 			if(cur != ",".charAt(0))
 			{
 				h	+= cur;
 			}else{
-				arr[idx++] = Integer.parseInt(h);
+				arr[counted++] = Integer.parseInt(h);
 				h = "";
 				
 			}
@@ -61,5 +111,10 @@ public class MainServer {
 		
 		return arr;
 	}
+	
+	
+	
+	
+	
 	}
 
--- client.py
+++ client.py
@@ -1,15 +1,79 @@
-
-import os
+import time
 import socket
-import subprocess
-from os import urandom
-from base64 import b64encode
+import serial
 from time import sleep
-import binascii
 import random
 import cgi,cgitb
 cgitb.enable() #for debugging
 oldArray = [0,0,0,0,0]
+ser = serial.Serial()
+ser.port = 'COM2'
+
+
+def getDataFromGlove():
+    inputSentence = ''
+    helpArray = [0,0,0,0,0]
+    iterations = 0
+    maxNumiteratorions = 100
+
+    counter = 0
+
+    #b'-1\r\n'
+    #b'255\r\n'
+    #b'255\r\n'
+    #b'255\r\n'
+    #b'255\r\n'
+    #b'255\r\n'
+    #b'-2\r\n'
+
+    while True:
+        read_serial = ser.readline().decode('utf-8')
+        lenOfNum = str.__len__(read_serial)
+        read_serial = read_serial.rstrip('\r\n')
+        #read_serial = read_serial[:lenOfNum - 2]  ##ommit lasttwo, they are just marker for next-line
+        # print('length of read: ' + int.__str__(lenOfNum))
+        # print("num:",read_serial)
+
+        if (read_serial == "-1"):
+            counter = 0
+           # print('hi!')
+        elif (read_serial == "-2"):
+            counter = 0
+        else:
+            if(counter>4):
+                print("counter too high ", counter)
+                counter = 4
+            if(read_serial != '' and '-' not in read_serial):
+                #print("read_serial: ", read_serial)
+                helpArray[counter] = int(read_serial)
+                counter += 1
+
+            iterations += 1
+            if (iterations == maxNumiteratorions):
+                n = 0
+                #for n in range(0, 5):
+                 #   helpArray[n] = int(helpArray[n] / maxNumiteratorions);
+                endTime = time.time()
+
+                print('perfect! collected enough values.. median is:')
+                n = 0
+                for num in helpArray:
+                    print(n, ": ", num)
+                    n += 1
+
+                n = 0
+
+                iterations = 0
+
+                for i in range(0, 5):
+                    #print(helpArray[i])
+                    inputSentence += int.__str__(helpArray[i]) + ","
+
+                counter = 0
+                return inputSentence
+
+
+
 
 def getRandomData(oldArray):
     randomSentence = ""
@@ -49,15 +113,18 @@ def getDataFromInput():
     inputString = input("Type your number: ")
     print(inputString)
 
+
+    helpArr = oldArray;
     if(inputString == "1"):
         helpArr = [random.randint(150,254), 0, 0, 0, 0]
     if(inputString == "2"):
-        helpArr = [0, random.randint(100, 254), random.randint(150, 254), 0, 0]
+        helpArr = [0, random.randint(150, 254), random.randint(150, 254), 0, 0]
     if (inputString == "3"):
         helpArr = [0,0, 0, 0, random.randint(150,254)]
     if (inputString == "4"):
         helpArr = [random.randint(150,254), random.randint(150,254), random.randint(150,254),
                      random.randint(150,254), random.randint(150,254)]
+
 
     counter = 0;
     for i in range(0, 5):
@@ -67,27 +134,32 @@ def getDataFromInput():
 
     return inputSentence
 
+def main():
+    mode = input("(t)est, (g)love or (r)andom mode?")
+    host = '127.0.0.1'
+    port = 9876
+    #ser = serial.Serial()
+    ser.open()
+    #BUFFER_SIZE = 1024
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
-mode = input("(t)est or (r)andom mode?")
-
-host = '127.0.0.1'
-while (1):
-    port = 6789
-    BUFFER_SIZE = 512
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("attempting to connect")
+    #s.connect((host, port))
+    #s.bind((host,port))
+    while True:
+        if(mode == 'r'):
+            bytes = getRandomData(oldArray)
+        if(mode == 't'):
+            bytes = getDataFromInput()
+        if(mode=='g'):
+            bytes = getDataFromGlove()
+        print("gathered data: " + bytes)
 
-    s.connect((host, port))
+        sock.sendto(str.encode(bytes), (host, port))
+        #s.sendall(str.encode(bytes))
+        print("Sending data")
+        #sleep(0.5)
 
-    if(mode == 'r'):
-        bytes = getRandomData(oldArray)
-    if(mode == 't'):
-        bytes = getDataFromInput()
-
-    print(bytes)
-
-
-    s.sendall(str.encode(bytes))
-    print("Sending data")
-    sleep(1)
-
+if __name__ == "__main__":
+    main()
